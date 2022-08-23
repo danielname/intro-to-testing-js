6d412446ee49a16f0d5d531b3e0480e5ca979d31
Daniel Name <daniel.v.name@gmail.com>
2022-08-23 12:46:09 -0500

    unit tests for edge cases.

--- tests.js
+++ tests.js
@@ -13,35 +13,49 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+// unit tests for the saHello function
 describe(`sayHello`, function() {
-    it('should be a defined function', function () {
-        expect(typeof sayHello).toBe('function');
-    });
-    it('should return a string', function () {
-        expect(typeof sayHello()).toBe(`string`);
-    });
-    it('should return `Hello, Jane!` when `Jane` is entered as an argument.', function () {
-        expect(sayHello(`Jane`)).toBe(`Hello, Jane!`);
-    });
-    it('should return `Hello, Alex!` when `Alex` is entered as an argument.', function () {
-        expect(sayHello(`Alex`)).toBe(`Hello, Alex!`);
-    });
-    it('should return `Hello, Pat!` when `Pat` is entered as an argument.', function () {
-        expect(sayHello(`Pat`)).toBe(`Hello, Pat!`);
-    });
-    it('should return any name that is inputted as an argument.', function () {
-        expect(sayHello(input)).toBe(`Hello, ${input}!`);
-    });
-    it('should return `Hello, World!` when the boolean `true` is entered as an argument.', function () {
-        expect(sayHello(true)).toBe(`Hello, World!`);
-    });
-    it('should return `Hello, World!` when the boolean `false` is entered as an argument.', function () {
-        expect(sayHello(false)).toBe(`Hello, World!`);
-    });
-    it('should return `Hello, World!` when the null is entered as an argument.', function () {
-        expect(sayHello(null)).toBe(`Hello, World!`);
-    });
-    it('should return `Hello, World!` when a number is entered as an argument.', function () {
-        expect(sayHello(4.2)).toBe(`Hello, World!`);
-    });
+    describe(`Core Functionality`, function(){
+        it('should be a defined function', function () {
+            expect(typeof sayHello).toBe('function');
+        });
+        it('should return a string', function () {
+            expect(typeof sayHello()).toBe(`string`);
+        });
+        it('should return `Hello, Jane!` when `Jane` is entered as an argument.', function () {
+            expect(sayHello(`Jane`)).toBe(`Hello, Jane!`);
+        });
+        it('should return `Hello, Alex!` when `Alex` is entered as an argument.', function () {
+            expect(sayHello(`Alex`)).toBe(`Hello, Alex!`);
+        });
+        it('should return `Hello, Pat!` when `Pat` is entered as an argument.', function () {
+            expect(sayHello(`Pat`)).toBe(`Hello, Pat!`);
+        });
+        it('should return any name that is inputted as an argument.', function () {
+            expect(sayHello(`fdoshfgorahgondf`)).toBe(`Hello, fdoshfgorahgondf!`);
+        });
+    })
+    describe(`Validation Test Suite`, function(){
+        it('should return `Hello, World!` when the boolean `true` is entered as an argument.', function () {
+            expect(sayHello(true)).toBe(`Hello, World!`);
+        });
+        it('should return `Hello, World!` when the boolean `false` is entered as an argument.', function () {
+            expect(sayHello(false)).toBe(`Hello, World!`);
+        });
+        it('should return `Hello, World!` when the null is entered as an argument.', function () {
+            expect(sayHello(null)).toBe(`Hello, World!`);
+        });
+        it('should return `Hello, World!` when a number is entered as an argument.', function () {
+            expect(sayHello(4.2)).toBe(`Hello, World!`);
+        });
+        it('should return `Hello, World!` when an empty string is entered as an argument.', function () {
+            expect(sayHello("")).toBe(`Hello, World!`);
+        });
+        it('should return `Hello, World!` when a numeric string is entered as an argument.', function () {
+            expect(sayHello("14812746")).toBe(`Hello, World!`);
+        });
+        it('should not return `undefined` when called', function () {
+            expect(sayHello()).not.toBe(undefined);
+        });
+    })
 })
